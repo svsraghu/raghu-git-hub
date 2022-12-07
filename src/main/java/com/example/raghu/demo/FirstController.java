ded352064836c431a200b56c0a08096fedbd1b31
svsraghu <svsrvardhan@gmail.com>
2022-12-07 11:29:12 +0530

    first

---
+++ src/main/java/com/example/raghu/demo/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.raghu.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "HELLO RAGHU";
+	}
+}
